718018acad3d1643cbc9de0cdb1e20fd3489b294
Adbhut <adbhut.rei.2233@gmail.com>
2024-02-05 20:35:36 +0530

    adding frontend

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { SubHeading } from '../components/SubHeading';
+import { Heading }from '../components/Heading'
+import { InputBox } from '../components/InputBox'
+import { Button } from '../components/Button'
+import axios from 'axios';
+
+export const Signup = ()=> {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setpassword] = useState("");
+    const nevigate = useNavigate();
+
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign up"} />
+          <SubHeading label={"Enter your infromation to create an account"} />
+          <InputBox onChange={(e) => {
+              setFirstName(e.target.value);
+            }} placeholder="John" label={"First Name"} 
+          />
+          <InputBox onChange={(e) => {
+              setLastName(e.target.value);
+            }} placeholder="Doe" label={"Last Name"}
+          />
+          <InputBox onChange={(e) => {
+              setUsername(e.target.value);
+            }} placeholder="harkirat@gmail.com" label={"Email"}
+          />
+          <InputBox onChange={(e) => {
+              setPassword(e.target.value);
+            }} placeholder="123456" label={"Password"}
+          />
+          <div className='pt-4'>
+            <Button onClick={ async ()=>{
+                const response = axios.post("http://localhost:3000/api/v1/user/signup", {
+                    username,
+                    firstName,
+                    lastName,
+                    password
+                });
+                localStorage.setItem("token", response.data.token)
+                navigate("/dashboard")
+            }} label={"Sign up"} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
